0d4d1e8d90f4307b8f3770d0b528492f3123bc0b
Miras_aka_Drak0n <159175540+MirasakaDrakon@users.noreply.github.com>
2025-12-28 16:57:46 +0500

    Refactor IP lookup script to use jq for JSON output

--- iprobiv.sh
+++ iprobiv.sh
@@ -6,31 +6,20 @@ echo "  | ||  __/| | | (_) | |_) | |\\ V / ";
 echo " |___|_|   |_|  \\___/|_.__/|_| \\_/  ";
 echo "                                    ";
 
-read -p "IP: " ip
-
+#!/bin/bash
+read -p "Enter IP: " ip
 echo "---------------------------------------"
-
 echo "Searching by ipinfo.io"
 curl -s https://ipinfo.io/"$ip"
-
 echo "---------------------------------------"
-
 echo "Searching by ipwho.is"
-curl -s https://ipwho.is/"$ip"?security=1
-
+curl -s https://ipwho.is/"$ip"?security=1 | jq
 echo "---------------------------------------"
-
 echo "Searching by api.ip2location.io"
-curl api.ip2location.io?ip="$ip"
-
+curl api.ip2location.io/?ip="$ip" | jq
 echo "---------------------------------------"
-
 echo "Searching by ip-api.com"
-curl ip-api.com/"$ip"
-
+curl ip-api.com/"$ip"?fields=66846719
 echo "---------------------------------------"
-
 echo "Searching by ipwhois.io"
-curl ipwhois.app/json/"$ip"
-
-read -p "Press Enter to continue..."
+curl ipwhois.app/json/"$ip" | jq
